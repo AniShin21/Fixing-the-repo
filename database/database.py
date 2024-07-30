7b155947eaddb6eb8bd7e413ced4c5354a08f7a3
AniShin21 <minecraft2727k@gmail.com>
2024-07-30 16:07:44 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -19,6 +19,28 @@ async def add_user(user_id: int):
     user_data.insert_one({'_id': user_id})
     return
 
+# Admins Database
+async def present_admin(user_id: int):
+    found = await admin_data.find_one({'_id': user_id})
+    return bool(found)
+
+
+async def add_admin(user_id: int):
+    user = new_user(user_id)
+    await admin_data.insert_one(user)
+    ADMINS.append(int(user_id))
+    return
+
+async def del_admin(user_id: int):
+    await admin_data.delete_one({'_id': user_id})
+    ADMINS.remove(int(user_id))
+    return
+
+async def full_adminbase():
+    user_docs = admin_data.find()
+    user_ids = [int(doc['_id']) async for doc in user_docs]
+    return user_ids
+
 
 async def full_userbase():
     user_docs = user_data.find()
