680e48950b3e4989a6ac6e6944b721c840803afe
AniShin21 <minecraft2727k@gmail.com>
2024-07-31 17:13:22 +0530

    Update top_anime.py

--- plugins/top_anime.py
+++ plugins/top_anime.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from bot import bot
+from helper_func import subscribed, decode, get_messages
 
 # Function to fetch top anime from MyAnimeList
 def fetch_top_anime():
