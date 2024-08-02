68455cb6716826ebf3e130fa2284eeef34b65728
AniShin21 <minecraft2727k@gmail.com>
2024-08-02 10:46:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from pyrogram.types import Message, CallbackQuery
 import pyrogram.utils
 import sys
 from datetime import datetime
-from config import API_HASH, APP_ID, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL1, FORCE_SUB_CHANNEL2, FORCE_SUB_CHANNEL3, FORCE_SUB_CHANNEL4, CHANNEL_ID, PORT
+from config import API_HASH, APP_ID, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL1, FORCE_SUB_CHANNEL2, FORCE_SUB_CHANNEL3, CHANNEL_ID, PORT
 from plugins.anime import top_anime, handle_callback
 
 # Set up logging
