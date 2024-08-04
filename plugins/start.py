518160af18da91421a2d10a8220fa4b7147bdb18
AniShin21 <minecraft2727k@gmail.com>
2024-08-04 21:17:33 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -73,7 +73,6 @@ async def start_command(client: Client, message: Message):
                 reply_markup = msg.reply_markup
             else:
                 reply_markup = None
-
             try:
                 snt_msg = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
                 await asyncio.sleep(0.5)
